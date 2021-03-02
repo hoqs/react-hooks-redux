67f1433392573dbc781cbecc2b14e3344c2efa1f
Henrique Oliveira Quevedo <henrique.quevedo@MLTBF.LOCAL>
2021-03-02 14:13:32 -0300

    feat: adicionada função shuffle e fillGrid

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,7 +1,7 @@
 export type GRID = [ROW, ROW, ROW, ROW, ROW, ROW, ROW, ROW, ROW]
 
-export type N = 0 | NUMBER
+export type N = 0 | NUMBERS
 
-export type NUMBER = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+export type NUMBERS = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 
 export type ROW = [N, N, N, N, N, N, N, N, N]
