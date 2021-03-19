44557d3aed49a7e9cb5930a0fc0d2e184008eaaf
Henrique Oliveira <henriqueoq98@gmail.com>
2021-03-19 14:55:37 -0300

    feat: configurando reducer

--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -1,10 +1,17 @@
-import { IReducer } from 'reducers'
 import { AnyAction } from 'redux'
+import { createFullGrid } from 'utils'
+import { IReducer } from './interfaces'
+import * as types from './types'
 
-const initialState = {}
+const initialState: IReducer = {}
 
 function reducer(state = initialState, action: AnyAction) {
   switch (action.type) {
+    case types.CREATE_GRID:
+      return {
+        ...state,
+        grid: createFullGrid(),
+      }
     default:
       return state
   }
