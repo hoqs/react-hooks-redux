64d9bbc97cdb01f22ad9cb6c9c7f4649318911c6
Henrique Oliveira <henriqueoq98@gmail.com>
2021-04-16 11:39:35 -0300

    feat: adicionado novo componente new game

---
+++ src/components/new-button/index.tsx
@@ -0,0 +1,18 @@
+import React, {FC, useCallback} from 'react'
+import {useDispatch} from 'react-redux'
+import {Action, Dispatch} from 'redux'
+
+import {Button} from 'components'
+
+import {createGrid} from 'reducers'
+
+const NewButton:FC = () =>{
+    const dispatch = useDispatch<Dispatch<Action>>()
+    const createNewGrid = useCallback(()=>{
+        if(window.confirm('Start a New Game?'))
+        dispatch(createGrid())
+    },[dispatch])
+    return <Button onClick={createNewGrid}>New Game</Button>
+}
+
+export default NewButton
